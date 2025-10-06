e83a737ea7e8f61488ee154a4d978c29f288614f
lakshya goyal (zero) <lakshyagoyal701@gmail.com>
2025-10-06 22:35:47 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,29 @@
+#Arithmatic Operators
+a = 7
+b = 5
+c = a + b
+d = a - b
+e = a * b
+print(c)
+
+#Assignment operator
+name = 20
+name +=40
+print(name)
+
+#Comparision operator - they always return a boolean value
+a = 10
+b = 40
+print(a>b)  # return false
+print(a!=b)  # not equals to willreturn true
+print(a==b)  # equals to will return false
+
+
+#Logical operators
+a = True or False
+print(a)
+a = True and False
+print(a)
+
+print(not(False))
+print(not(True))
