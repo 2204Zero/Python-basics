54a792c588e11fa05d58a87e4abcf598df5e1e7b
lakshya goyal (zero) <lakshyagoyal701@gmail.com>
2025-10-06 22:25:02 +0530

    Variables and Data types in python

---
+++ Variables&DataTypes.py
@@ -0,0 +1,11 @@
+a = 1
+b = 2.2
+print(a+b) 
+
+c = "ZERO"
+print(c)
+
+# print(a+c)  it will throw an error
+d = False    #boolean variable
+e = None     #none type variable 
+# 1g = 20 zero@name = 230 throw an error due to variable defining rules
