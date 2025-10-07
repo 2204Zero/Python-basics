fdcf332d57858cce75b9bded64f0223a79f5c9d9
lakshya goyal (zero) <lakshyagoyal701@gmail.com>
2025-10-07 20:56:03 +0530

    changes in strin file

--- Strings.py
+++ Strings.py
@@ -2,15 +2,12 @@ name = "ZERO"
 
 a = name[1:7]  #last index value is not included
 print(a)
-print(name[-1]) #never use negative index (although they work but they create confusion)
+print(name[-1:6]) #never use negative index (although they work but they create confusion)
 print(name[1:])  #it is same as [1:4]
 print(name[:4])  #it is same as [0:4] 
 
 word = "Amazing"
 a = word[0:5:2]
-print(a)
-
-
 # String's functions
 print(len(word))
 
