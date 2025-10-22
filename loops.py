314fa88e5baded91213cd90f5181c5a168233093
lakshya goyal (zero) <lakshyagoyal701@gmail.com>
2025-10-22 15:31:35 +0530

    loops in python

---
+++ loops.py
@@ -0,0 +1,13 @@
+# Loops in python
+# for loop 
+a = 100
+for i in range(a):
+    print("Hello")
+
+# While loop 
+i = 1    # for while loop we have to initiate the iterator
+while (i<a):      #Correct 
+    print("hello")
+    i+=1   # we have to update the iterator, so that it doesn't go into infinite loop
+# while i<a:      #incorrect
+#     print("hello") 
